9f28b6d4ed71f4be28bba685d5f33d20ece7d749
Ethan Thompson <ecthompson99@gmail.com>
2020-10-16 15:50:38 -0400

    Added doxygen boilerplate information and main function with sample dataset

--- assignment-1/stats.c
+++ assignment-1/stats.c
@@ -8,3 +8,33 @@
  * misuse of this material.
  *
  *****************************************************************************/
+/**
+ * @file stats.c
+ * @author Ethan Thompson
+ * @date 15 October 2020
+ * @brief File which implements statistical operations on a dataset.
+ *
+ * Takes an array of unsigned char data items and returns the max, min, mean,
+ * and median of the dataset.
+ *
+ * @see https://www.coursera.org/learn/introduction-embedded-systems
+ */
+
+ #include <stdio.h>
+ #include <stats.h>
+
+ // Define size of the dataset
+ #define DATASET_SIZE 40
+
+
+ int main() {
+
+ // Dataset from Alex Fosdick's sample implementation
+ unsigned char test[DATASET_SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                                     114,  88,  45,  76, 123,  87,  25,  23,
+                                     200, 122, 150,  90,  92,  87, 177, 244,
+                                     201,   6,  12,  60,   8,   2,   5,  67,
+                                       7,  87, 250, 230,  99,   3, 100,  90};
+
+
+ }
